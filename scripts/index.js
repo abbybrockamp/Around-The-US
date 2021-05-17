03095433efc73a56c948277b3d19508ca88e650f
Abby Brockamp <abby.brockamp@gmail.com>
2021-05-17 14:22:37 -0700

    feat: edit form js. Profile name and title populate in form, and form changes inputs on profile. Auto closes and default reset.

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,36 @@
-const editModalEl = document.querySelector(".edit-btn-modal");
-const profileEditBtnEl = document.querySelector(".profile__edit-btn")
-const closeBtnEl = document.querySelector(".edit-btn-modal__close-btn");
+let editModalEl = document.querySelector(".edit-btn-modal");
+let profileEditBtnEl = document.querySelector(".profile__edit-btn")
+let closeBtnEl = document.querySelector(".edit-btn-modal__close-btn");
+
+let editForm = document.querySelector(".edit-form");
+let editFormNameInput = document.querySelector(".edit-form__name");
+let editFormTitleInput = document.querySelector(".edit-form__title");
+
+let profileNameEl = document.querySelector(".profile__name");
+let ProfileTitleEl = document.querySelector(".profile__title");
+
+function openModal() {
+    editModalEl.classList.add("edit-btn-modal_open");
+}
+
+function closeModal() {
+    editModalEl.classList.remove("edit-btn-modal_open");
+}
 
 profileEditBtnEl.addEventListener("click", function() {
-    editModalEl.classList.add("edit-btn-modal_open")
+    openModal();
 });
 
 closeBtnEl.addEventListener("click", function() {
-    editModalEl.classList.remove("edit-btn-modal_open")
+    closeModal();
+});
+
+editFormNameInput.value = profileNameEl.textContent;
+editFormTitleInput.value = ProfileTitleEl.textContent;
+
+editForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    profileNameEl.textContent = editFormNameInput.value;
+    ProfileTitleEl.textContent = editFormTitleInput.value;
+    closeModal();
 });
