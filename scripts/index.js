7e2b35ee5be76756bd77bb7b33a21f8ee5559d63
Abby Brockamp <abby.brockamp@gmail.com>
2021-05-19 15:32:41 -0700

    refactor: improved html and bem structures

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,22 @@
-let editModalEl = document.querySelector(".edit-btn-modal");
+let editModalEl = document.querySelector(".modal");
 let profileEditBtnEl = document.querySelector(".profile__edit-btn")
-let closeBtnEl = document.querySelector(".edit-btn-modal__close-btn");
+let closeBtnEl = document.querySelector(".modal__close-btn");
 
 let editForm = document.querySelector(".edit-form");
-let editFormNameInput = document.querySelector(".edit-form__name");
-let editFormTitleInput = document.querySelector(".edit-form__title");
+let editFormNameInput = document.querySelector(".edit-form__input_content_name");
+let editFormTitleInput = document.querySelector(".edit-form__input_content_title");
 
 let profileNameEl = document.querySelector(".profile__name");
 let ProfileTitleEl = document.querySelector(".profile__title");
 
 function openModal() {
-    editModalEl.classList.add("edit-btn-modal_open");
+    editModalEl.classList.add("modal_open");
     editFormNameInput.value = profileNameEl.textContent;
     editFormTitleInput.value = ProfileTitleEl.textContent;
 }
 
 function closeModal() {
-    editModalEl.classList.remove("edit-btn-modal_open");
+    editModalEl.classList.remove("modal_open");
 }
 
 function submittedModal(event) {
